d2b3bd89416e88f6413d56fc5f293c14472f6be2
Aguilera450 <aguilera@ciencias.unam.mx>
2022-09-17 02:05:55 -0500

    UPDATE: se anexa ejercicio 2 completo.

--- Practica02/Practica02.rkt
+++ Practica02/Practica02.rkt
@@ -14,7 +14,7 @@ deben tener los constructores indicados
 (define-type Figura
   [paralelogramo (a number?) (b number?) (h number?)]
   [triangulo (a number?) (b number?) (c number?)]
-  [rombo (l number?) (d number?) (D number?)]
+  [rombo (l number?) (D number?) (d number?)]
   [rectangulo (a number?) (b number?)]
   [elipse (a number?) (b number?)])
 
@@ -29,15 +29,36 @@ Definir las funciones area y perimetro.
 
 (define (perimetro f)
   (cond
-    [(elipse? f)
-     (* pi (- (* 3 (+ (elipse-a f) (elipse-b f)))
-              (sqrt (* (+ (elipse-b f) (* 3 (elipse-a f)))
-                       (+ (elipse-a f) (* 3 (elipse-b f)))))))]
     [(paralelogramo? f)
      (+ (* 2 (paralelogramo-a f)) (* 2 (paralelogramo-b f)))]
     [(triangulo? f)
      (+ (triangulo-a f) (+ (triangulo-b f) (triangulo-c f)))]
     [(rectangulo? f)
      (+ (* 2 (rectangulo-a f)) (* 2 (rectangulo-b f)))]
+    [(elipse? f)
+     (let*([a (elipse-a f)]
+           [b (elipse-b f)])
+       (* pi (- (* 3 (+ a b))
+                (sqrt (* (+ b (* 3 a))
+                         (+ a (* 3 b)))))))]
     [(rombo? f)
      (* 4 (rombo-l f))]))
+
+;; area: Figura →  number
+
+(define (area f)
+  (cond
+    [(triangulo? f)
+     (let*([s (/ (perimetro f) 2)]
+           [a (triangulo-a f)]
+           [b (triangulo-b f)]
+           [c (triangulo-c f)])
+       (sqrt (* s (* (- s a) (* (- s b) (- s c))))))]
+    [(paralelogramo? f)
+     (* (paralelogramo-a f) (paralelogramo-h f))]
+    [(rectangulo? f)
+     (* (rectangulo-a f) (rectangulo-b f))]
+    [(elipse? f)
+     (* pi (* (elipse-a f) (elipse-b f)))]
+    [(rombo? f)
+     (/ (* (rombo-D f) (rombo-d f)) 2)]))
