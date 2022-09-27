55fbd68a7414e3aa842a425efaf76acad0fda0a3
Marco Rivera Silva <marcoantonioriverasilva@ciencias.unam.mx>
2022-09-26 20:10:16 -0500

    Medio DOcumentado

--- Practica02/Practica02.rkt
+++ Practica02/Practica02.rkt
@@ -4,222 +4,222 @@
 ;; Adrian Aguilera Moreno 421005200
 ;; Kevin Jair Torres Valencia 318331818
 
-#| 
-             Ejercicio 1:
-Definir un tipo abstracto Figura que sea
-utilizado para trabajar con geométricas,
-deben tener los constructores indicados
-|#
-
+;;Pregunta 1
+;; Definimos el tipo de dato Figura
 (define-type Figura
-  [paralelogramo (a number?) (b number?) (h number?)]
   [triangulo (a number?) (b number?) (c number?)]
-  [rombo (l number?) (D number?) (d number?)]
   [rectangulo (a number?) (b number?)]
+  [rombo (l number?) (D number?) (d number?)]
+  [paralelogramo (a number?) (b number?) (h number?)]
   [elipse (a number?) (b number?)])
 
-#|
-            Ejercicio 2:
-Definir las funciones area y perimetro.
-|#
-;; Definiendo π:
-(define pi 3.141592653589793238)
 
-;; perimetro: Figura →  number
-
-(define (perimetro f)
+;;Pregunta 2
+;; Perimetros
+;; Funcion que obtiene el area de las figuras
+(define (perimetro figura)
   (cond
-    [(paralelogramo? f)
-     (+ (* 2 (paralelogramo-a f)) (* 2 (paralelogramo-b f)))]
-    [(triangulo? f)
-     (+ (triangulo-a f) (+ (triangulo-b f) (triangulo-c f)))]
-    [(rectangulo? f)
-     (+ (* 2 (rectangulo-a f)) (* 2 (rectangulo-b f)))]
-    [(elipse? f)
-     (let*([a (elipse-a f)]
-           [b (elipse-b f)])
-       (* pi (- (* 3 (+ a b))
-                (sqrt (* (+ b (* 3 a))
-                         (+ a (* 3 b)))))))]
-    [(rombo? f)
-     (* 4 (rombo-l f))]))
+    [(triangulo? figura) (perTr figura) ]
+    [(rectangulo? figura) (perRec figura)]
+    [(rombo? figura) (perRom figura)]
+    [(paralelogramo? figura) (perPar figura)]
+    [(elipse? figura) (perEli figura)]))
 
-;; area: Figura →  number
+;; Funcion auxiliar de perimetro que sirve para calcular el perimetro del Triangulo
+(define (perTr triangulo)
+  (+ (triangulo-a triangulo) (triangulo-b triangulo) (triangulo-c triangulo)))
 
-(define (area f)
+;; Funcion auxiliar de perimetro que sirve para calcular el perimetro del Rectangulo
+(define (perRec rectangulo)
+  (+ (* 2 (rectangulo-a rectangulo)) (* 2 (rectangulo-b rectangulo))))
+
+;; Funcion auxiliar de perimetro que sirve para calcular el perimetro del Rombo
+(define (perRom rombo)
+  (* 4 (rombo-l rombo)))
+
+;; Funcion auxiliar de perimetro que sirve para calcular el perimetro del Paralelogramo
+(define (perPar para)
+  (+ (* 2 (paralelogramo-a para)) (* 2 (paralelogramo-b para))))
+
+;; Funcion auxiliar de perimetro que sirve para calcular el perimetro de la Elipse
+(define (perEli eli)
+  (* pi (- (* 3 (+ (elipse-a eli) (elipse-b eli))) (sqrt (* (+ (* 3 (elipse-a eli)) (elipse-b eli)) (+ (elipse-a eli) (* 3 (elipse-b eli))))))))
+
+;; Areas
+
+;; Funcion que obtiene el area de las figuras
+(define (area figura)
   (cond
-    [(triangulo? f)
-     (let*([s (/ (perimetro f) 2)]
-           [a (triangulo-a f)]
-           [b (triangulo-b f)]
-           [c (triangulo-c f)])
-       (sqrt (* s (* (- s a) (* (- s b) (- s c))))))]
-    [(paralelogramo? f)
-     (* (paralelogramo-a f) (paralelogramo-h f))]
-    [(rectangulo? f)
-     (* (rectangulo-a f) (rectangulo-b f))]
-    [(elipse? f)
-     (* pi (* (elipse-a f) (elipse-b f)))]
-    [(rombo? f)
-     (/ (* (rombo-D f) (rombo-d f)) 2)]))
+    [(triangulo? figura) (arTr figura) ]
+    [(rectangulo? figura) (arRec figura)]
+    [(rombo? figura) (arRom figura)]
+    [(paralelogramo? figura) (arPar figura)]
+    [(elipse? figura) (arEli figura)]))
 
-#|
-                       Ejercicio 3:
-El tipo de datos Vagon, que debe incluir 4 constructores;
-uno por cada tipo de vagón descrito en el manual de prácticas:
-|#
-;; Tipo de dato Vagon y constructores
+;; Funcion auxiliar de area que sirve para calcular el area del Triangulo
+(define (arTr tri)
+  (let*([s (/ (+ (triangulo-a tri) (triangulo-b tri) (triangulo-c tri)) 2)])
+    (sqrt (* s (- s (triangulo-a tri)) (- s (triangulo-b tri)) (- s (triangulo-c tri))))))
 
+;; Funcion auxiliar de area que sirve para calcular el area del Rectangulo
+(define (arRec rec)
+  (* (rectangulo-a rec) (rectangulo-b rec)))
+
+;; Funcion auxiliar de area que sirve para calcular el area del Rombo
+(define (arRom rom )
+  (/ (* (rombo-D rom) (rombo-d rom)) 2))
+
+;; Funcion auxiliar de area que sirve para calcular el area del Paralelogramo
+(define (arPar para)
+  (* (paralelogramo-b para) (paralelogramo-h para)))
+
+;; Funcion auxiliar de area que sirve para calcular el area de la elipse
+(define (arEli eli)
+  (* pi (elipse-a eli) (elipse-b eli)))
+
+;; Pregunta 3
 (define-type Vagon
-  [dormitorio (camas positive-integer?)]
-  [locomotora (p     positive-integer?)]
-  [pasajeros  (cap   positive-integer?)]
-  [restaurante
-   (mesas    positive-integer?)
-   (personal positive-integer?)])
+  [locomotora (p positive-integer?)]
+  [pasajeros (cap positive-integer?)]
+  [restaurante (mesas positive-integer?) (personal positive-integer?)]
+  [dormitorio (camas positive-integer?)])
 
-;; Tipo de dato Tren
+;; Definimos un predicado para permitir una creacion de trenes similar a la de los ejemplos
+;; (Perdon por el nombre xd)
+(define (alv? a)
+  (if (or (Tren? a) (Vagon? a))
+      #t
+      #f))
 
-#|     Ejercicio 4
-|#
-
-;; Es el segundo punto de los ejercicios del 4.
+;; Definimos el tipo de dato Tren
 (define-type Tren
-  [tren-loc (vagon Vagon?)]
-  [tren (loci locomotora?)
-        (resto Tren?)
-        (locd locomotora?)]
-  [tren-t (vagon Vagon?)
-        (resto Tren?)]
-  [tren-f (resto Tren?)
-        (vagon Vagon?)]
-  )
+  [tren-loc (a locomotora?)]
+  [tren-f (a Tren?) (b alv?)])
 
-(define (ac-potloc tren)
-  (cond
-    [(tren-loc? tren)(let (
-         [vagon (tren-loc-vagon tren)])
-         (if (locomotora? vagon)
-             (locomotora-p vagon)
-             0))]
-    [(tren? tren)(let (
-         [loci (tren-loci tren)]
-         [locd (tren-locd tren)]
-         [restot (tren-resto tren)])
-         (+ (locomotora-p loci)
-            (locomotora-p locd)
-            (ac-potloc restot)))]
-    [(tren-t? tren)(let (
-         [vagon (tren-t-vagon tren)]
-         [restot (tren-t-resto tren)])
-         (+ (ac-potloc restot)
-         (if (locomotora? vagon)
-             (locomotora-p vagon)
-             0)))]
-    [(tren-f? tren)(let (
-         [restot (tren-f-resto tren)]
-         [vagon (tren-f-vagon tren)])
-         (+ (ac-potloc restot)
-            (if (locomotora? vagon)
-                (locomotora-p vagon)
-                0)))])
-  )
 
-(define (cuenta-vagones-noloc tren)
-  (cond
-    [(tren-loc? tren)(let (
-         [vagon (tren-loc-vagon tren)])
-         (if (locomotora? vagon)
-             0
-             1))]
-         [(tren? tren)(let (
-              [loci (tren-loci tren)]
-              [locd (tren-locd tren)]
-              [restot (tren-resto tren)])
-              (cuenta-vagones-noloc restot))]
-         [(tren-t? tren)(let (
-              [vagon (tren-t-vagon tren)]
-              [resto (tren-t-resto tren)])
-              (+ (cuenta-vagones-noloc resto)
-              (if (locomotora? vagon)
-                  0
-                  1)))]
-         [(tren-f? tren)(let (
-              [vagon (tren-f-vagon tren)]
-              [restot (tren-f-resto tren)])
-              (+ (cuenta-vagones-noloc restot)
-              (if(locomotora? vagon)
-                 0
-                 1)))])
-  )
+;; Pregunta 4
+;; Funcion que cuenta el numero de pasajeros del tren
+(define (num-pasajeros tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (pasajeros? y)
+                (+ (num-pasajeros x) (pasajeros-cap y))
+                (+ (num-pasajeros x) 0))]))
 
+;; Funcion que cuenta el arrastre usado por el tren
 (define (arrastre-usado tren)
-  (/ (* (cuenta-vagones-noloc tren) 100 ) (ac-potloc tren))
- )
+  (cond
+    [(eq? (contar-vagones tren) 0) (* (contar-locomotoras tren) 100)]
+    [else (* (/ (contar-vagones tren) (contar-locomotoras tren)) 100)]))
 
-;; ==================================== Pruebas unitarias ====================================
-;;                                        Perimetros:
-;; Triángulo
-(define (prueba-perimetro-triangulo1)
-  (test (perimetro (triangulo 2 2 2)) 6))
+;; Funcion auxiliar de arrastre-tren que cuenta la potencia de la locomotoras del tren
+(define (contar-locomotoras tren)
+  (type-case Tren tren
+    [tren-loc (x) (locomotora-p (tren-loc-a tren))]
+    [tren-f (x y)
+            (if (tren-loc? y)
+                (+ (contar-locomotoras x) (locomotora-p (tren-loc-a y)))
+                (+ (contar-locomotoras x) 0))]))
 
-(define (prueba-perimetro-triangulo2)
-  (test (perimetro (triangulo 1 2 3)) 6))
+;; Funcion auxiliar de arrastre-tren que cuenta la cantidad de vagones del tren
+(define (contar-vagones tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (Vagon? y)
+                (+ (contar-vagones x) 1)
+                (+ (contar-vagones x) 0))]))
 
-;; Rectángulo
-(define (prueba-perimetro-rectangulo1)
-  (test (perimetro (rectangulo 2 2)) 8))
+;; Funcion que cuenta el numero de personas que se quedan si camita en el tren
+(define (sin-cama tren)
+  (if (>= (contar-camas tren) (contar-pasajeros tren))
+      (- (contar-camas tren) (contar-pasajeros tren))
+      (- (contar-pasajeros tren) (contar-camas tren))))
 
-(define (prueba-perimetro-rectangulo2)
-  (test (perimetro (rectangulo 1 3)) 8))
+;; Funcion auxiliar de sin-cama que cuenta las camas del tren
+(define (contar-camas tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (dormitorio? y)
+                (+ (contar-camas x) (dormitorio-camas y))
+                (+ (contar-camas x) 0))]))
 
-;; Rombo
-(define (prueba-perimetro-rombo)
-  (test (perimetro (rombo 3 4 2)) 12))
+;; Funcion auxiliar de sin-cama que cuenta el numero de pasajeros del tren
+(define (contar-pasajeros tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (pasajeros? y)
+                (+ (contar-pasajeros x) (pasajeros-cap y))
+                (+ (contar-pasajeros x) 0))]))
 
-;; Paralelogramo
-(define (prueba-perimetro-paralelogramo1)
-  (test (perimetro (paralelogramo 2 2 2)) 8))
+;; Funcion que cuenta el numero de comensales maximos que se pueden atender en el tren
+(define (max-comensales tren)
+  (if (>= (* 8 (contar-personal tren)) (* 4 (contar-mesas tren)))
+      (* 4 (contar-mesas tren))
+      (* 8 (contar-personal tren))))
 
-(define (prueba-perimetro-paralelogramo2)
-  (test (perimetro (paralelogramo 4 2 2)) 12))
+;; Funcion auxiliar de max-comensales que cuenta el personal del tren
+(define (contar-personal tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (restaurante? y)
+                (+ (contar-personal x) (restaurante-personal y))
+                (+ (contar-personal x) 0))]))
 
-;; Elipse
-(define (prueba-perimetro-elipse1)
+
+;; Funcion auxiliar de max-comensales que cuenta las mesas del tren
+(define (contar-mesas tren)
+  (type-case Tren tren
+    [tren-loc (x) 0]
+    [tren-f (x y)
+            (if (restaurante? y)
+                (+ (contar-mesas x) (restaurante-mesas y))
+                (+ (contar-mesas x) 0))]))
+
+;;Pruebas Unitarias
+
+(define (prueba-perimetro-Triangulo)
+  (test (perimetro (triangulo 3 4 5)) 12))
+
+(define (prueba-area-Triangulo)
+  (test (area (triangulo 3 4 5)) 6))
+
+(define (prueba-perimetro-Rectangulo)
+  (test (perimetro (rectangulo 5 6)) 22))
+
+(define (prueba-area-Rectangulo)
+  (test (area (rectangulo 5 6)) 30))
+
+(define (prueba-perimetro-Rombo)
+  (test (perimetro (rombo 3 6 8)) 12))
+
+(define (prueba-area-Rombo)
+  (test (area (rombo 3 6 8)) 24))
+
+(define (prueba-perimetro-Paralelogramo)
+  (test (perimetro (paralelogramo 3 6 8)) 18))
+
+(define (prueba-area-Paralelogramo)
+  (test (area (paralelogramo 3 6 8)) 48))
+
+(define (prueba-perimetro-Elipse)
   (test (perimetro (elipse 3 3)) 18.84955592153876))
 
-(define (prueba-perimetro-elipse2)
-  (test (perimetro (elipse 4 3)) 22.103491790916742))
-
-;;                                          Áreas:
-;; Triángulo
-(define (prueba-area-triangulo1)
-  (test (area (triangulo 2 2 2)) 1.7320508075688772))
-
-(define (prueba-area-triangulo2)
-  (test (area (triangulo 1 2 3)) 0))
-
-;; Rectángulo
-(define (prueba-area-rectangulo1)
-  (test (area (rectangulo 2 2)) 4))
-
-(define (prueba-area-rectangulo2)
-  (test (area (rectangulo 1 4)) 4))
-
-;; Rombo
-(define (prueba-area-rombo)
-  (test (area (rombo 3 4 2)) 4))
-
-;; Paralelogramo
-(define (prueba-area-paralelogramo1)
-  (test (area (paralelogramo 2 2 2)) 4))
-
-(define (prueba-area-paralelogramo2)
-  (test (area (paralelogramo 4 2 2)) 8))
-
-;; Elipse
-(define (prueba-area-elipse1)
+(define (prueba-area-Elipse)
   (test (area (elipse 3 3)) 28.274333882308138))
 
-(define (prueba-area-elipse2)
-  (test (area (elipse 4 3)) 37.69911184307752))
+(define (prueba-num-pasajeros)
+  (test (num-pasajeros (tren-f (tren-f (tren-loc (locomotora 4)) (pasajeros 15)) (pasajeros 16))) 31))
+
+(define (prueba-arrastre-usado)
+  (test (arrastre-usado (tren-f (tren-f (tren-f (tren-loc (locomotora 1)) (pasajeros 10)) (restaurante 5 2)) (dormitorio 10))) 300))
+
+(define (prueba-sin-cama)
+  (test (sin-cama (tren-f (tren-f (tren-loc (locomotora 1)) (dormitorio 20)) (pasajeros 40))) 20))
+  
+(define (prueba-max-comensales)
+  (test (max-comensales (tren-f (tren-f (tren-loc (locomotora 1)) (restaurante 7 3)) (pasajeros 10))) 24))
