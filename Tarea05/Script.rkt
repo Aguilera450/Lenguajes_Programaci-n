4ecf08e806da060d000ad17ee8d8a80c8337ddc4
Aguilera450 <aguilera@ciencias.unam.mx>
2022-11-06 15:44:25 -0600

    UPDATE: se anexa bosquejo de ejercicio 4. A modificar.

--- Tarea05/Script.rkt
+++ Tarea05/Script.rkt
@@ -12,3 +12,19 @@
       (cons (cons (first list2) (ocurrencia list1 (first list2)))
             (ocurrencias list1 (rest list2))))
 )
+
+; Aquí se definirán los algoritmos con recursión de cola:
+(define (ocurrencias list1 list2)
+  (define (ocurrencia list1 x)
+    (if (empty? list1)
+        0
+        (if(equal? x (first list1))
+           (+ 1 (ocurrencia (rest list1) x))
+           (ocurrencia (rest list1) x)))
+  )
+  
+  (if (empty? list2)
+      empty
+      (cons (cons (first list2) (ocurrencia list1 (first list2)))
+            (ocurrencias list1 (rest list2))))
+)
